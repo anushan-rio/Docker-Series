84d3cacf24b8fe01c3dd573ca46cc7d3c7f5119d
Ansuhan <anushan559@gmail.com>
2024-10-03 15:58:53 +0530

    Node Project container creation

---
+++ Node-Project-Conatiner/index.js
@@ -0,0 +1,8 @@
+const express=require("express");
+const app=express();
+
+app.get('/',(req,res)=>{
+    res.send("<h1>Hello</h1>");
+})
+
+app.listen(8000,()=>{console.log("Port is Runing Sucessfully")})
